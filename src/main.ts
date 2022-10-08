d6fd5864c98f2f866316e07f179853f61ba12cc7
Muzaffar <xmuzaffar1999@gmail.com>
2022-10-08 22:45:35 +0500

    cors problem

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['*'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
 }
